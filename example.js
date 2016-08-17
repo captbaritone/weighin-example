8f9f0a423b2c4778483005c530eb3ab844bcac48
Jordan Eldredge <jordan@jordaneldredge.com>
2016-08-16 17:57:48 -0700

    Make example.js uglifyable

--- example.js
+++ example.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+if (false) {
+    console.log('Something has gone terribly wrong');
+} else {
+    var message = 'All is right with the world';
+    console.log(message);
+}
